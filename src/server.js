cc8674b2afc8c01b18e75e6ae332e059c1e9cff5
Veljo Lasn <vlasn@tlu.ee>
2017-05-03 17:48:29 +0300

    Accidentally removed Validateur

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const bodyParser = require("body-parser")
+const Validateur = require("./lib/validateur.js")
 require("dotenv").config()
 const app = express()
 
